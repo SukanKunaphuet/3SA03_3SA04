5e2b293206e38ebc10559b234f373d9fa9c99356
SukanKunaphuet <6310110518@email.psu.ac.th>
2022-08-09 22:35:31 +0700

    third commit

--- src/App.js
+++ src/App.js
@@ -24,13 +24,15 @@ function App() {
 
 export default App; */
 
-import React from 'react';
-import './App.css';
+import CharacterCard from './CharacterCard';
+const word = "Hello"
 
 function App() {
   return (
     <div>
-      Hello {"World"}
+      {
+        Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+      }
     </div>
   );
 }
